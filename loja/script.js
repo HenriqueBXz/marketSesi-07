a61ed383878e34f6b68cf53f157df30a2cf24ece
Nicole-Alcantara <alcantara.nicole.jesus@gmail.com>
2025-08-07 11:19:46 -0300

    Add files via upload

---
+++ loja/script.js
@@ -0,0 +1,10 @@
+let produtos
+
+window.onload = function(){
+    var storedUser = localStorage.getItem("usuario")
+    var user = JSON.parse(storedUser)
+
+    document.getElementById("user").innerText = user.name
+    document.getElementById("perfil").innerText = user.dataEntrada
+    document.getElementById("idPerfil").innerText = user.id
+};
